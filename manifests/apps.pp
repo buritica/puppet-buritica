dcf224e802fc14118a510ef63bf26935b784c576
Juan Pablo Buritica <buritica@gmail.com>
2015-03-30 00:42:53 -0400

    [fixt] application path for brew cask

--- manifests/apps.pp
+++ manifests/apps.pp
@@ -6,13 +6,13 @@ class buritica::apps {
   package {'alfred': provider => 'brewcask', install_options => "--appdir=/Applications"}
   package {'minecraft': provider => 'brewcask', install_options => "--appdir=/Applications"}
   package {'spotify': provider => 'brewcask', install_options => "--appdir=/Applications"}
-  package {'limechat': provider => 'brewcask', install_options => "--appdir='/Applications'"}
-  package {'imageoptim': provider => 'brewcask', install_options => "--appdir='/Applications'"}
-  package {'iterm2': provider => 'brewcask', install_options => "--appdir='/Applications'"}
-  package {'vlc': provider => 'brewcask', install_options => "--appdir='/Applications'"}
-  package {'vmware-fusion': provider => 'brewcask', install_options => "--appdir='/Applications'"}
+  package {'limechat': provider => 'brewcask', install_options => "--appdir=/Applications"}
+  package {'imageoptim': provider => 'brewcask', install_options => "--appdir=/Applications"}
+  package {'iterm2': provider => 'brewcask', install_options => "--appdir=/Applications"}
+  package {'vlc': provider => 'brewcask', install_options => "--appdir=/Applications"}
+  package {'vmware-fusion': provider => 'brewcask', install_options => "--appdir=/Applications"}
 
-  # package {'totalfinder': provider => 'brewcask', install_options => "--appdir='/Applications'"}
+  # package {'totalfinder': provider => 'brewcask', install_options => "--appdir=/Applications"}
 
   exec {'brew cask alfred link':
     require => Package['alfred']
