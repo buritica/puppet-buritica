4a5453fe9fe8d7928e02c28d86f28578d785d1c4
Juan Pablo Buritica <buritica@gmail.com>
2015-03-30 00:09:31 -0400

    [update] sublime settings

--- manifests/sublime.pp
+++ manifests/sublime.pp
@@ -2,19 +2,21 @@ class buritica::sublime {
   notice('setting up buritica sublime text')
 
   # install app
-  include sublime_text
+  # include sublime_text
+  package {'sublime-text3': provider => 'brewcask', install_options => "--binarydir=/opt/boxen/bin"}
 
   # install package control
   sublime_text::package { 'Package Control':
     source => 'wbond/sublime_package_control'
   }
 
-  # sync sublime text user folder if it
-  # exists in dropbox
+  # install inconsolata font
+  package {'font-inconsolata': provider => 'brewcask'}
+
+  # sync sublime text user from dropbox
   file { '/Users/buritica/Library/Application Support/Sublime Text 3/Packages/User':
-    ensure  => symlink,
-    force   => true,
-    target  => '/Users/buritica/Dropbox/Sync/ST/User'
+    ensure  => 'link',
+    target  => '/Users/buritica/Dropbox (Personal)/Sync/ST/User/'
   }
 
 }
